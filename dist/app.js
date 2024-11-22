7c0c2eb0dc2edd66a6e796d31ff6a5b4587d6ecd
ayan-akd <ayankumar.akd@gmail.com>
2024-11-22 18:13:18 +0600

    basic project setup done

--- dist/app.js
+++ dist/app.js
@@ -9,6 +9,8 @@ const app = (0, express_1.default)();
 app.use(express_1.default.json());
 app.use((0, cors_1.default)());
 app.get('/', (req, res) => {
+    const a = 10;
     res.send('Hello World!');
+    res.send(a);
 });
 exports.default = app;
